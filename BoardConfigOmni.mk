e049841ed402c3d331f6df46572f4c9ae3ce72ad
zawzaww <mczawzaw12@outlook.com>
2017-11-14 17:29:31 +0630

    TWRP: update inline kernel source location and add brightness twrp flags
    Changes to be committed:
    	modified:  angler/BoardConfigOmni.mk
    
    Signed-off-by: ZawZaw <mczawzaw12@outlook.com>

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -1,7 +1,7 @@
-# Inline kernel
+# Inline kernel by PureZ
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-TARGET_KERNEL_SOURCE := kernel/huawei/angler
-TARGET_KERNEL_CONFIG := angler_defconfig
+TARGET_KERNEL_SOURCE := kernel/huawei/purez-kernel-angler
+TARGET_KERNEL_CONFIG := purez_defconfig
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
@@ -36,4 +36,6 @@ RECOVERY_SDCARD_ON_DATA := true
 BOARD_HAS_NO_REAL_SDCARD := true
 TW_INCLUDE_CRYPTO := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 215
+TW_DEFAULT_BRIGHTNESS := 115
 TARGET_RECOVERY_QCOM_RTC_FIX := true
