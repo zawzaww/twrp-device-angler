2675f72a86ebd58741f9350384f801f663a54263
zawzaww <mczawzaw12@outlook.com>
2017-11-15 18:20:05 +0630

    TWRP flags: add Target Second CUP architecture
    Changes to be committed:
    	modified:   BoardConfigOmni.mk
    
    Signed-off-by: ZawZaw <mczawzaw12@outlook.com>

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -13,12 +13,19 @@ TARGET_USES_UNCOMPRESSED_KERNEL := true
 TARGET_BOARD_PLATFORM := msm8994
 TARGET_BOOTLOADER_BOARD_NAME := angler
 
-# CUP Architecture
+# Target CUP Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a53
+
+# Target Second CPU Architecture
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a7
 
 # F2FS filesystem Support
 TARGET_USERIMAGES_USE_F2FS := true
