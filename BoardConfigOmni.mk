f38728ec0af33b83304ef7f843431859855c6991
zawzaww <mczawzaw12@outlook.com>
2017-11-15 16:28:03 +0630

    TWRP BoardConfig: update twrp flags and add TWRP ZawZaw Addition
    Changes to be committed:
    	modified:   /angler/BoardConfigOmni.mk
    
    Signed-off-by: ZawZaw <mczawzaw12@outlook.com>

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -1,6 +1,7 @@
-# Inline kernel by PureZ
+# TWRP Inline Kernel: PureZ Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/huawei/purez-kernel-angler
+# PureZ Kernel config
 TARGET_KERNEL_CONFIG := purez_defconfig
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
@@ -10,35 +11,38 @@ TARGET_USES_UNCOMPRESSED_KERNEL := true
 # F2FS filesystem Support
 TARGET_USERIMAGES_USE_F2FS := true
 
+# TWRP Theme
+TW_THEME := portrait_hdpi
+
+# TWRP
+BOARD_SUPPRESS_SECURE_ERASE := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+
 # Camera
 BOARD_USES_AOSP_CAMERA := true
-
 # Use device's audio_effects.conf
 TARGET_USE_DEVICE_AUDIO_EFFECTS_CONF := true
-
 # Ignore vendor audio_effects.conf
 TARGET_IGNORE_VENDOR_AUDIO_EFFECTS_CONF := true
-
 # Enable vendor image symlink
 BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
-
 # Skip keymaster compilation
 TARGET_PROVIDES_KEYMASTER := true
-
 # Needed for VoLTE
 AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
-
 # Increase coldboot timeout
 TARGET_INCREASES_COLDBOOT_TIMEOUT := true
 
-# TWRP
-TW_THEME := portrait_hdpi
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-BOARD_SUPPRESS_SECURE_ERASE := true
-RECOVERY_SDCARD_ON_DATA := true
-BOARD_HAS_NO_REAL_SDCARD := true
+# TWRP ZawZaw Addition
 TW_INCLUDE_CRYPTO := true
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+RECOVERY_SDCARD_ON_DATA := true
 TW_MAX_BRIGHTNESS := 215
-TW_DEFAULT_BRIGHTNESS := 115
-TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_DEFAULT_BRIGHTNESS := 100
+TARGET_RECOVERY_DEVICE_DIRS := device/huawei/angler
+TW_EXCLUDE_SUPERSU := true
+TW_NEVER_UNMOUNT_SYSTEM := true
+TW_NO_SCREEN_TIMEOUT := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_DEVICE_VERSION := angler by ZawZaw
